05e111827d351b80bbe3f79bada817b0afeb7de3
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-08-07 14:04:25 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+MONGO_URI = os.getenv('MONGO_URI')
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = MONGO_URI
+mongo = PyMongo(app)
+
+@app.route('/')
+def front_page():
+  return render_template('index.html')
