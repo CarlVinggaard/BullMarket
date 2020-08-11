280eea887bf854cbebb3f425219f79040145a4ad
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-08-12 00:30:50 +0200

    Add templates and basic routes and navigation

--- app.py
+++ app.py
@@ -9,5 +9,17 @@ app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 
 @app.route('/')
+def portfolio():
+  return render_template('portfolio.html')
+
+@app.route('/history')
+def history():
+  return render_template('history.html')
+
+@app.route('/trade')
+def trade():
+  return render_template('trade.html')
+
+@app.route('/frontpage')
 def front_page():
-  return render_template('base.html')
+  return render_template('frontpage.html')
