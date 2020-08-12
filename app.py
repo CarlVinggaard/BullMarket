1c4e43627370cc932c0b384188d503bdf4dd17d4
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-08-12 22:13:34 +0200

    Connect to database and enable user creation

--- app.py
+++ app.py
@@ -1,25 +1,49 @@
 import os
-from flask import Flask, render_template, request
+import logging
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_pymongo import PyMongo
 
 MONGO_URI = os.getenv('MONGO_URI')
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = MONGO_URI
+app.config["MONGO_URI"] = "mongodb+srv://carlvinggaard:18M0n90d603@cluster0-lqr5z.mongodb.net/stockTradingGame?retryWrites=true&w=majority"
+app.secret_key = 'secretkey'
 mongo = PyMongo(app)
 
-@app.route('/')
-def portfolio():
-  return render_template('portfolio.html')
+def create_user(username):
+  mongo.db.users.insert({ 'username': username, 'cash': 20000.00 })
+  return 
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+  if 'username' in session:
+    return render_template('portfolio.html', user=mongo.db.users.find_one({ 'username': session['username'] }))
+  else:
+    if request.method == 'POST':
+      username = request.form['username'].lower()
+      session['username'] = username
+      exists = mongo.db.users.find_one({ 'username': username })
+      if not exists:
+        create_user(username)
+      return redirect(url_for('index'))
+    return render_template('frontpage.html')
 
 @app.route('/history')
 def history():
-  return render_template('history.html')
+  return render_template('history.html', user=mongo.db.users.find_one({ 'username': session['username'] }))
 
 @app.route('/trade')
 def trade():
-  return render_template('trade.html')
+  return render_template('trade.html', stocks=mongo.db.stocks.find())
 
-@app.route('/frontpage')
-def front_page():
-  return render_template('frontpage.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    session['username'] = request.form['username']
+    return redirect(url_for('index'))
+  return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+  session.pop('username', None)
+  return redirect(url_for('index'))
