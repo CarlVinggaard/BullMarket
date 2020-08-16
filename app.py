99d7aa69bd64fcfcf221193be6fa36bf12f266c4
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-08-16 16:27:35 +0200

    Import environment variables

--- app.py
+++ app.py
@@ -161,3 +161,10 @@ def sell(stockCode):
 def logout():
   session.pop('username', None)
   return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+            
