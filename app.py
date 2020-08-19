7460a8b663a0218b81fa603cb97ff883be4a3220
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-08-19 15:49:42 +0200

    Add docstring comments

--- app.py
+++ app.py
@@ -4,23 +4,29 @@ from utils import *
 
 app = create_flask_app()
 
+''' ROUTES '''
+
 @app.before_request
 def before():
   if 'username' not in session and request.endpoint != 'index':
     return redirect(url_for('index'))
 
-''' ROUTES '''
 @app.route('/', methods=['POST', 'GET'])
 def index():
+  ''' If logged in, go to portfolio, else go to front page'''
   if 'username' in session:
-    return render_template('portfolio.html', data=get_stock_data(), user=mongo.db.users.find_one({ 'username': session['username'] }), value=get_total_value(session['username']))
+    return render_template('portfolio.html', data=get_stock_data(), user=mongo.db.users.find_one({ 'username': session['username'] }),
+      value=get_total_value(session['username']))
   else:
     if request.method == 'POST':
       username = request.form['username'].lower()
       session['username'] = username
+
+      ''' Try to find an existing user, otherwise create one. '''
       exists = mongo.db.users.find_one({ 'username': username })
       if not exists:
         create_user(username)
+
       return redirect(url_for('index'))
     return render_template('frontpage.html')
 
@@ -44,7 +50,11 @@ def buy(stock_code):
   if request.method == "POST":
     quantity = int(request.form["quantity"])
     total = quantity * price
+
+    ''' If 'buy' is not in the form, we are simply calculating the price. '''
     if 'buy' in request.form:
+
+      ''' Either purchase the stock or set an error. '''
       if is_valid_purchase(quantity, price, user['cash']):
         buy_stock(stock_code, quantity, price)
         update_value_at_last_trade()
@@ -67,14 +77,19 @@ def sell(stock_code):
   if request.method == "POST":
     quantity = int(request.form["quantity"])
     total = quantity * price
+
+    ''' If 'sell' is not in the form, we are simply calculating the price. '''
     if 'sell' in request.form:
+
+      ''' Either sell the stock or set an error. '''
       if is_valid_sale(quantity, stock_quantity):
         sell_stock(stock_code, quantity, price)
         update_value_at_last_trade()
         return redirect(url_for('index'))
       else:
         error = "You don't have that much of this stock."
-  return render_template('sell.html', user=user, stock=stock_code, price=price, total=total, quantity=quantity, error=error, stock_quantity=stock_quantity)
+  return render_template('sell.html', user=user, stock=stock_code, price=price, total=total, quantity=quantity, error=error,
+    stock_quantity=stock_quantity)
 
 @app.route('/stocks/<stock_code>', methods=['GET', 'POST'])
 def stock(stock_code):
@@ -83,6 +98,10 @@ def stock(stock_code):
   stock = mongo.db.stocks.find_one({ 'stockCode': stock_code })
   comments = mongo.db.comments.find({ 'stockCode': stock_code }).sort('createdAt')
   editing_id = ''
+  ''' If 'delete' -> delete,
+      if 'edit' -> update a comment and stop editing,
+      if 'editing' -> set editing state,
+      if 'comment' -> post comment. '''
   if request.method == 'POST':
     if 'delete' in request.form:
       delete_comment(request.form['delete'])
--- utils.py
+++ utils.py
@@ -13,23 +13,27 @@ def create_flask_app():
 
 mongo = PyMongo(create_flask_app())
 
-# FUNCTIONS
+''' FUNCTIONS '''
+
+''' Get data for all stocks in database, using the yfinance API. '''
 def get_stock_data():
   stock_code_array = []
   stocks = mongo.db.stocks.find()
   for stock in stocks:
     stock_code_array.append(stock['stockCode'])
   
-  # Get data from API
+  ''' Get data from API. '''
   data = yf.download(stock_code_array, period="1d")
   get_stock_tuple = lambda code: (code, round(data[("Close", code)][0], 2))
   stock_price_dict = dict(map(get_stock_tuple, stock_code_array))
   return stock_price_dict
 
+''' Get price for a single stock. '''
 def get_stock_price(stock_code):
   data = yf.download(stock_code, period="d1")
   return round(data["Close"][0], 2)
 
+''' Get the total value of a user's stock portfolio (excluding cash). '''
 def get_total_value(username):
   user = mongo.db.users.find_one({ 'username': username })
   data = get_stock_data()
@@ -38,6 +42,7 @@ def get_total_value(username):
 
   return round(sum(list(map(get_stock_value, user['portfolio']))), 2)
 
+''' Get a dictionary mapping all stocks to the number of comments on them. '''
 def get_comment_counts(data):
   comments = {}
   for stock in data:
@@ -45,50 +50,54 @@ def get_comment_counts(data):
     comments[stock] = count
   return comments
     
-
+''' Return a boolean describing if a user has cash for a purchase. '''
 def is_valid_purchase(quantity, price, cash):
   return quantity * price <= cash
 
+''' Return a boolean describing if a user has enough stock for a sale '''
 def is_valid_sale(quantity, stock_quantity):
   return stock_quantity >= quantity
 
+''' Add a trade to the database. '''
 def add_trade(stock_code, quantity, buy_or_sell, price):
   trade = { 'stockCode': stock_code, 'quantity': quantity, 'type':  buy_or_sell, 'price': price, 'timestamp': datetime.now() }
   mongo.db.users.update_one({ 'username': session['username'] }, { '$push': { 'trades': trade } })
 
+''' Add stock to a user portfolio and subtract the cash. '''
 def buy_stock(stock_code, quantity, price):
-  # If there is no object with this stock code in the portfolio, create one
+  ''' If there is no object with this stock code in the portfolio, create one. '''
   mongo.db.users.update({ 'username': session['username'], 'portfolio.stockCode': { '$ne': stock_code } }, { '$push': { 'portfolio': { 'stockCode': stock_code, 'quantity': 0 } } })
  
-  # Increment the portfolio with <quantity>
+  ''' Increment the portfolio with <quantity>. '''
   query = { 'username': session['username'], 'portfolio.stockCode': stock_code }
   value = { '$inc': { 'portfolio.$.quantity': quantity } }
   mongo.db.users.update_one(query, value)
 
-  # Subtract the cash
+  ''' Subtract the cash. '''
   mongo.db.users.update({ 'username': session['username']}, { '$inc': { 'cash': -(quantity * price) } })
 
-  # Add trade to history
+  ''' Add trade to history. '''
   add_trade(stock_code, quantity, 'buy', price)
 
+''' Subtract the stock from a user portfolio and add the cash. '''
 def sell_stock(stock_code, quantity, price):
-  # Decrement the portfolio with <quantity>
+  ''' Decrement the portfolio with <quantity>. '''
   query = { 'username': session['username'], 'portfolio.stockCode': stock_code }
   value = { '$inc': { 'portfolio.$.quantity': -quantity } }
   mongo.db.users.update_one(query, value)
 
-  # Add the cash
+  ''' Add the cash. '''
   mongo.db.users.update({ 'username': session['username']}, { '$inc': { 'cash': quantity * price } })
 
-  # Add trade to history
+  ''' Add trade to history. '''
   add_trade(stock_code, quantity, 'sell', price)
 
+''' Update the valueAtLastTrade for a user. '''
 def update_value_at_last_trade():
   user = mongo.db.users.find_one({ 'username': session['username'] })
   cash = user['cash']
   total = get_total_value(session['username']) + cash
   mongo.db.users.update_one({ 'username': session['username'] }, { '$set': { 'valueAtLastTrade': total } })
-
 
 def create_user(username):
   mongo.db.users.insert({ 'username': username, 'cash': 20000.00, 'portfolio': [] })
